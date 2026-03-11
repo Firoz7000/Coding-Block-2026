f44bceed2ba50caf2a378d83a3176970eedce48c
Firoz Khan <firozkhan@Firozs-MacBook-Air.local>
2026-03-11 23:11:13 +0530

    Add New Leacture

---
+++ Leacture 12 (11 -3- 26)/896_monotonicarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int inc = true;
+        int dec = true;
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i]>nums[i-1])dec = false;
+            if(nums[i]<nums[i-1])inc = false;
+        }
+        return inc || dec;
+    }
+};
